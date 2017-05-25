f2e314755c5c711ebbe5b4e2db4d05221eabf6c0
Andreas Christ <andreas.christ@sixt.com>
2017-05-25 11:49:27 +0200

    replaced Ray by Line

--- src/main/java/de/avwc/util/RayTracer.java
+++ src/main/java/de/avwc/util/RayTracer.java
@@ -11,18 +11,20 @@ import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
  * Created by andichrist on 23.04.17.
  */
 public class RayTracer {
-    private Camera camera = Scene.getInstance().getCamera();
 
-    public void trace(Display display) {
+    public static void trace(Display display) {
+        Scene scene = Scene.getInstance();
+        Camera camera = scene.getCamera();
+
         // u = l + (r − l)(i + 0.5)/nx
         // v = b + (t − b)(j + 0.5)/ny
         // l = left, r = right, b = bottom, t = top
-        for (int i = 0; i < Main.WIDTH; i++) {
-            for (int j = 0; j < Main.HEIGHT; j++) {
+        for (int i = 0; i < scene.getWidth(); i++) {
+            for (int j = 0; j < scene.getHeight(); j++) {
                 // from left to right
-                double u = camera.getLeft() + (camera.getRight() - camera.getLeft()) * (i + 0.5) / Main.WIDTH;
+                double u = camera.getLeft() + (camera.getRight() - camera.getLeft()) * (i + 0.5) / scene.getWidth();
                 // from top to bottom
-                double v = camera.getTop() + (camera.getBottom() - camera.getTop()) * (j + 0.5) / Main.HEIGHT;
+                double v = camera.getTop() + (camera.getBottom() - camera.getTop()) * (j + 0.5) / scene.getHeight();
 
                 // direction from camera to current pixel
                 Vector3D s = Vector3DUtil.add(camera.getU().scalarMultiply(u), camera.getV().scalarMultiply(v), camera.getW_d_negated());
@@ -35,4 +37,5 @@ public class RayTracer {
             }
         }
     }
+
 }
