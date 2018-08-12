84aa6dd20913e2801705582b8a9db0c465a128f8
Andreas Christ <andreas.christ@sixt-leasing.com>
2018-08-12 09:11:05 +0200

    add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module de.avwc {
+    // export packages
+    exports de.avwc.util;
+    exports de.avwc;
+
+    // import modules
+    requires javafx.base;
+    requires javafx.graphics;
+    requires java.logging;
+    requires java.desktop;
+    requires com.fasterxml.jackson.databind;
+    requires commons.math3;
+    requires jackson.annotations;
+    requires com.fasterxml.jackson.core;
+}
