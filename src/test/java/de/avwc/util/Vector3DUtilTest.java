b21f4fb03b9d4f9d9bbeec84b30ad01ce886e14d
Andreas Christ <andreas.christ@sixt-leasing.com>
2018-08-25 12:25:20 +0200

    some tests added

--- src/test/java/de/avwc/util/Vector3DUtilTest.java
+++ src/test/java/de/avwc/util/Vector3DUtilTest.java
@@ -16,58 +16,103 @@ import static org.junit.Assert.assertEquals;
  */
 public class Vector3DUtilTest {
 
-    private static double DELTA = 10e-12;
+    private static double Δ = 10e-12;   // DELTA
 
     @Test
     public void testQuadraticFormula1() {
-        Set<Double> solutions = Vector3DUtil.quadraticFormula(4, -12, -40);
-        Iterator iterator = solutions.iterator();
-
+        Set<Double> solutions = Vector3DUtil.quadraticFormula(4, -12, -40); // 4x² - 12x - 40
+        //List<Double> sorted = solutions.stream().sorted().collect(Collectors.toList());
         assertEquals(solutions.size(), 2);
 
+        Iterator iterator = solutions.iterator();
         double t0 = (double) iterator.next();
         double t1 = (double) iterator.next();
 
         double min = min(t0, t1);
         double max = max(t0, t1);
 
-        assertEquals(min, -2.0, DELTA);
-        assertEquals(max, 5.0, DELTA);
+        assertEquals(min, -2.0, Δ);
+        assertEquals(max, 5.0, Δ);
     }
 
     @Test
     public void testQuadraticFormula2() {
-        Set<Double> solutions = Vector3DUtil.quadraticFormula(1, 2, -35);
-        Iterator iterator = solutions.iterator();
-
+        Set<Double> solutions = Vector3DUtil.quadraticFormula(1, 2, -35); // x² + 2x - 35
         assertEquals(solutions.size(), 2);
 
+        Iterator iterator = solutions.iterator();
         double t0 = (double) iterator.next();
         double t1 = (double) iterator.next();
 
         double min = min(t0, t1);
         double max = max(t0, t1);
 
-        assertEquals(min, -7.0, DELTA);
-        assertEquals(max, 5.0, DELTA);
+        assertEquals(min, -7.0, Δ);
+        assertEquals(max, 5.0, Δ);
     }
 
     @Test
     public void testQuadraticFormula3() {
-        Set<Double> solutions = Vector3DUtil.quadraticFormula(1, -4, 4);
-        Iterator iterator = solutions.iterator();
-
+        Set<Double> solutions = Vector3DUtil.quadraticFormula(1, -4, 4); // x² - 4x + 4
         assertEquals(solutions.size(), 1);
 
+        Iterator iterator = solutions.iterator();
         double t0 = (double) iterator.next();
 
-        assertEquals(t0, 2.0, DELTA);
+        assertEquals(t0, 2.0, Δ);
     }
 
     @Test
     public void testQuadraticFormula4() {
-        Set<Double> solutions = Vector3DUtil.quadraticFormula(1, 12, 37);
+        Set<Double> solutions = Vector3DUtil.quadraticFormula(1, 12, 37); // x² + 12x + 37
         assertEquals(solutions.size(), 0);
+    }
+
+    @Test
+    public void testQuadraticFormula5() {
+        Set<Double> solutions = Vector3DUtil.quadraticFormula(1, 0, -100);  // x² - 100
+        assertEquals(solutions.size(), 2);
+
+        Iterator iterator = solutions.iterator();
+        double t0 = (double) iterator.next();
+        double t1 = (double) iterator.next();
+
+        double min = min(t0, t1);
+        double max = max(t0, t1);
+
+        assertEquals(min, -10.0, Δ);
+        assertEquals(max, 10.0, Δ);
+    }
+
+    @Test
+    public void testQuadraticFormula6() {
+        Complex expected1 = new Complex(-Math.sqrt(3d/25d), 0);
+        Complex expected0 = new Complex(Math.sqrt(3d/25d), 0);
+
+        double[] coefficients = {-3, 0, 25}; // 25x² - 3 = 0
+
+        Set<Double> solutions = Vector3DUtil.quadraticFormula(coefficients);
+        assertEquals(solutions.size(), 2);
+
+        Iterator iterator = solutions.iterator();
+        double t0 = (double) iterator.next();
+        double t1 = (double) iterator.next();
+
+        double min = min(t0, t1);
+        double max = max(t0, t1);
+
+        assertEquals(min, expected1.getReal(), Δ);
+        assertEquals(max, expected0.getReal(), Δ);
+
+        // LaguerreSolver
+        LaguerreSolver solver = new LaguerreSolver();
+        Complex[] result = solver.solveAllComplex(coefficients, 0);
+
+        Complex actual0 = result[0];
+        assertSame(expected0, actual0);
+
+        Complex actual1 = result[1];
+        assertSame(expected1, actual1);
     }
 
     @Test
@@ -75,7 +120,7 @@ public class Vector3DUtilTest {
         Complex expected0 = new Complex(-2, 0);
         Complex expected1 = new Complex(5, 0);
 
-        double[] coefficients = {-40, -12, 4};
+        double[] coefficients = {-40, -12, 4}; // 4x² - 12x - 40
         LaguerreSolver solver = new LaguerreSolver();
         Complex[] result = solver.solveAllComplex(coefficients, 0);
 
@@ -92,7 +137,7 @@ public class Vector3DUtilTest {
         Complex expected0 = new Complex(5, 0);
         Complex expected1 = new Complex(-7, 0);
 
-        double[] coefficients = {-35, 2, 1};
+        double[] coefficients = {-35, 2, 1}; // x² + 2x - 35
         LaguerreSolver solver = new LaguerreSolver();
         Complex[] result = solver.solveAllComplex(coefficients, 0);
 
@@ -107,7 +152,7 @@ public class Vector3DUtilTest {
     public void testMath3_3() {
         Complex expected = new Complex(2, 0);
 
-        double[] coefficients = {4, -4, 1};
+        double[] coefficients = {4, -4, 1}; // x² - 4x + 4
         LaguerreSolver solver = new LaguerreSolver();
         Complex[] result = solver.solveAllComplex(coefficients, 0);
 
@@ -125,7 +170,7 @@ public class Vector3DUtilTest {
         Complex expected0 = new Complex(-6, -1);
         Complex expected1 = new Complex(-6, 1);
 
-        double[] coefficients = {37, 12, 1};
+        double[] coefficients = {37, 12, 1}; // x² + 12x + 37
         LaguerreSolver solver = new LaguerreSolver();
         Complex[] result = solver.solveAllComplex(coefficients, 0);
 
@@ -137,7 +182,7 @@ public class Vector3DUtilTest {
     }
 
     private static void assertSame(double expected, double actual) {
-        assertEquals(expected, actual, DELTA);
+        assertEquals(expected, actual, Δ);
     }
 
     private static void assertSame(Complex expected, Complex actual) {
